06573ec66ff942c714192ce9a2b9a9bc6ba7bbef
yxyx136 <linus.schilpp@googlemail.com>
2020-04-18 09:23:14 +0200

    feat: save currently focused tab

--- background/background.js
+++ background/background.js
@@ -1,18 +1,18 @@
 let defaultWorkspaces = [
-  { color: '#2f2f2f', tabs: [''] },
-  { color: '#903216', tabs: [''] },
-  { color: '#d24091', tabs: [''] },
-  { color: '#7f8c8d', tabs: [''] },
-  { color: '#e0e0e0', tabs: [''] },
-  { color: '#7f40d2', tabs: [''] },
-  { color: '#360f96', tabs: [''] },
-  { color: '#c71a1c', tabs: [''] },
-  { color: '#fe7511', tabs: [''] },
-  { color: '#fadc10', tabs: [''] },
-  { color: '#40d252', tabs: [''] },
-  { color: '#3ce0de', tabs: [''] },
-  { color: '#187efc', tabs: [''] },
-  { color: '#1c18fc', tabs: [''] },
+  { color: '#2f2f2f', tabs: [''], highlightedTab: 0 },
+  { color: '#903216', tabs: [''], highlightedTab: 0 },
+  { color: '#d24091', tabs: [''], highlightedTab: 0 },
+  { color: '#7f8c8d', tabs: [''], highlightedTab: 0 },
+  { color: '#e0e0e0', tabs: [''], highlightedTab: 0 },
+  { color: '#7f40d2', tabs: [''], highlightedTab: 0 },
+  { color: '#360f96', tabs: [''], highlightedTab: 0 },
+  { color: '#c71a1c', tabs: [''], highlightedTab: 0 },
+  { color: '#fe7511', tabs: [''], highlightedTab: 0 },
+  { color: '#fadc10', tabs: [''], highlightedTab: 0 },
+  { color: '#40d252', tabs: [''], highlightedTab: 0 },
+  { color: '#3ce0de', tabs: [''], highlightedTab: 0 },
+  { color: '#187efc', tabs: [''], highlightedTab: 0 },
+  { color: '#1c18fc', tabs: [''], highlightedTab: 0 },
 ]
 
 let workspaces, currentWorkspace
@@ -56,8 +56,16 @@ function switchToWorkspace(i) {
 }
 
 function saveOpenTabs(oldIndex, callback) {
-  chrome.tabs.query({ currentWindow: true }, (tabs) => {
-    workspaces[oldIndex].tabs = tabs.map(tab => tab.url)
+  chrome.tabs.query({}, (tabs) => {
+    let tabURLs = []
+    for (let i=0; i<tabs.length; i++) {
+      let tab = tabs[i]
+      tabURLs.push(tab.url)
+      if (tab.highlighted) {
+        workspaces[oldIndex].highlightedTab = i
+      }
+    }
+    workspaces[oldIndex].tabs = tabURLs
     chrome.storage.local.set({workspaces})
     let oldTabs = tabs.map(tab => tab.id)
     if (typeof callback !== 'undefined') callback(oldTabs)
@@ -72,13 +80,17 @@ function removeTabs(tabs) {
 
 function loadNewTabs() {
   let ws = workspaces[currentWorkspace]
-  for (let tabURL of ws.tabs) {
-    if (tabURL == '') {
-      chrome.tabs.create({})
-    } else {
-      chrome.tabs.create({ url: tabURL })
+  for (let i=0; i<ws.tabs.length; i++) {
+    let tabURL = ws.tabs[i]
+    let params = {}
+    if (tabURL != '') {
+      params['url'] = tabURL 
     }
+    chrome.tabs.create(params)
   }
+  setTimeout(() => {
+    chrome.tabs.highlight({ tabs: ws.highlightedTab })
+  }, 100)
 }
 
 function setBrowserIconColor(color) {
