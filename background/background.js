1960e53572b0dcd2f48d76b1c2283b6ab9f278a2
yxyx136 <linus.schilpp@googlemail.com>
2020-04-18 08:34:48 +0200

    feat: add more workspaces

--- background/background.js
+++ background/background.js
@@ -1,12 +1,18 @@
 let defaultWorkspaces = [
-  { color: '#3498db', tabs: [''] },
-  { color: '#e67e22', tabs: [''] },
-  { color: '#f1c40f', tabs: [''] },
-  { color: '#27ae60', tabs: [''] },
-  { color: '#9b59b6', tabs: [''] },
-  { color: '#e74c3c', tabs: [''] },
-  { color: '#34495e', tabs: [''] },
+  { color: '#2f2f2f', tabs: [''] },
+  { color: '#903216', tabs: [''] },
+  { color: '#d24091', tabs: [''] },
   { color: '#7f8c8d', tabs: [''] },
+  { color: '#e0e0e0', tabs: [''] },
+  { color: '#7f40d2', tabs: [''] },
+  { color: '#360f96', tabs: [''] },
+  { color: '#c71a1c', tabs: [''] },
+  { color: '#fe7511', tabs: [''] },
+  { color: '#fadc10', tabs: [''] },
+  { color: '#40d252', tabs: [''] },
+  { color: '#3ce0de', tabs: [''] },
+  { color: '#187efc', tabs: [''] },
+  { color: '#1c18fc', tabs: [''] },
 ]
 
 let workspaces, currentWorkspace
